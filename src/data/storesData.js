2ad847221dc031d42124553c2699596cd3ac68a9
raeedalhendy <raeedalhendy.4@gmail.com>
2026-02-24 03:55:36 +0300

    Updated PlatformHome + dynamic stores

--- src/data/storesData.js
+++ src/data/storesData.js
@@ -1,17 +1,18 @@
-
+import candleimage from '../image/candleimage.jpg'
+import watchimage from '../image/watchimage.png'
 
 export const stores = [
   {
     id: "candles",
     title: "Candles Glow Store",
     description: "شموع فاخرة وروائح طبيعية.",
-    image: "../../public/candleg.jpg",
+    image: "../image/candleimage.jpg",
   },
   {
     id: "watches",
     title: "متجر الساعات",
     description: "ساعات أنيقة وعصرية.",
-    image: "../../public/watchg.png",
+    image: "../image/watchimage.png",
   },
 
 ];
